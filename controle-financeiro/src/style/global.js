182ad4b0090c45450ddb59b0f3b83893d7f2d4c0
davi <davijluna@gmail.com>
2026-04-14 19:59:09 -0300

    arquivos de base para o projeto

---
+++ controle-financeiro/src/style/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+   * {
+    margin: 0;
+    padding: 0;
+    }
+
+    body {
+        font-family: 'Poppins', Sans-serif;
+        background-color: #f1e7e7ff;
+    }
+`;
+
+export default Global;
