f0517c50768883d8f291292a2c65df1ae5ad4659
davi <davijluna@gmail.com>
2026-05-03 18:37:00 -0300

    adicionando valores no localStorage : saída e entrada de valores no app

--- controle-financeiro/src/components/Form/index.jsx
+++ controle-financeiro/src/components/Form/index.jsx
@@ -1,14 +1,18 @@
 // import React from 'react'
 
-import React, { useState } from "react"
+import { useState } from "react"
+
+
 
 import * as C from "./style";
 
-const Form = () => {
+const Form = ({ handleAdd }) => {
 
   const [desc, setDesc] = useState("");  
   const [amount, setAmount] = useState("");
   const [isExpense, setExpense] = useState(false);
+
+  const generateID = () => Math.round(Math.random() * 1000);
 
   const handleSave = () => {
     if(!desc || !amount) {
@@ -18,6 +22,19 @@ const Form = () => {
         alert("O valor tem que ser positivo!");
         return;
     }
+
+    const transaction = {
+        id: generateID(),
+        desc: desc,
+        amount: amount,
+        expense: isExpense,
+    };
+
+    handleAdd(transaction);
+
+    setDesc("");
+    setAmount("");
+
   };
 
   return (
